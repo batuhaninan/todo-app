283ad5fdf1488e137e6daf098eea48a9373320f2
batuhaninan <inanbatuhan61@gmail.com>
2021-09-23 01:16:52 +0300

    added context

--- todo-frontend/src/index.js
+++ todo-frontend/src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import useTodos from "./TodoHooks";
+import TodoContext from "./TodoContext";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+export const baseUrl = process.env.CLIENT_API_BASE_URL || "http://localhost:8080/api";
 
-export const baseUrl = "http://localhost:8080/api";
+const Index = () => {
+  const sampleTodo = useState([{
+    id: 1,
+    text: "",
+    isFinished: false,
+    user: "",
+    createdAt: "",
+    updatedAt: "",
+    userId: 1,
+}]) 
+
+  const todos = useTodos(useState([]))
+
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <TodoContext.Provider value={todos}>
+          <App />
+        </TodoContext.Provider>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Index />, rootElement);
