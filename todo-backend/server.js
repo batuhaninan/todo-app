d939bdb2c1eda5f5cdf7f783924fdf72355b785b
batuhaninan <inanbatuhan61@gmail.com>
2021-09-23 01:17:51 +0300

    added dotenv

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -1,12 +1,13 @@
+require("dotenv").config();
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.NODE_DOCKER_PORT || 8080;
 
 var corsOptions = {
-    origin: ["http://localhost:8081", "http://localhost:3000"]
+    origin: [process.env.CLIENT_ORIGIN || "http://localhost:8081", "http://localhost:3000"]
 }
 
 app.use(cors(corsOptions));
